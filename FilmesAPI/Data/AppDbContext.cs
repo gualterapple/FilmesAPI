a98a22a5aca7f93eb59c6992b640330b0f470de4
gualterapple <39972000+gualterapple@users.noreply.github.com>
2021-09-22 08:41:30 +0100

    Começando o modulo II

---
+++ FilmesAPI/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using FilmesAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesAPI.Data.DTOs
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> opt): base (opt)
+        {
+        }
+
+        DbSet<Filme> Filmes { get; set; }
+        DbSet<Cinema> Cinemas { get; set; }
+    }
+}
--- FilmesAPI/Models/Cinema.cs
+++ FilmesAPI/Models/Cinema.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,10 @@ namespace FilmesAPI.Models
 {
     public class Cinema
     {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "O campo nome é obrigatório")]
+        public string Nome { get; set; }
     }
 }
